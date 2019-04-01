433bea33d3977839b31a25b65e4ddef2a4039ae0
Eric Darve <darve@stanford.edu>
2019-04-01 14:13:37 -0700

    minor

--- Code/ex1_transform.cpp
+++ Code/ex1_transform.cpp
@@ -9,6 +9,11 @@ Compile with:
 g++ -std=c++11 -o ex1_transform ex1_transform.cpp
 */
 
+/*
+See
+https://en.cppreference.com/w/cpp/iterator/iterator
+for information about iterators.
+*/
 struct range_iterator
 {
   public:
@@ -22,6 +27,7 @@ struct range_iterator
 
     range_iterator &operator++()
     {
+        // Increment counter
         /* todo */
         return *this;
     }
@@ -40,6 +46,10 @@ struct range_iterator
     unsigned i;
 };
 
+/* Similar to an STL container but "contains" only a sequence of integers
+from 0 to n-1.
+https://en.cppreference.com/w/cpp/container
+*/
 class range
 {
   public:
@@ -82,8 +92,13 @@ int main(void)
     print_vector(a2);
 
     /*
+    See
+    https://en.cppreference.com/w/cpp/algorithm/transform
+
     OutputIt transform( InputIt1 first1, InputIt1 last1, InputIt2 first2, 
                     OutputIt d_first, BinaryOperation binary_op );
+
+    Should output vector with entries a1[i] + a2[i]
     */
     std::transform(a1.begin(), a1.end(),
                    a2.begin(),
@@ -93,16 +108,15 @@ int main(void)
     cout << "a1+a2:\n";
     print_vector(out);
 
-    const unsigned n = 4;
-
-    range r(n);
-    vector<int> multiple_of(n);
+    range r(a1.size());
+    vector<int> multiple_of(a1.size());
 
     /*
     OutputIt transform(InputIt first1, InputIt last1, OutputIt d_first,
                        UnaryOperation unary_op);
+
+    Should output vector with entries 3*i
     */
-    // Should output vector with entries 3*i
     std::transform(r.begin(), r.end(),
                    multiple_of.begin(),
                    [](int i) { return /* todo */ 0; });
@@ -111,7 +125,7 @@ int main(void)
     print_vector(multiple_of);
 
     // Should output vector with a1[i] * a2[i]
-    // Use your range variable instead of dummy
+    // Use your range variable r instead of dummy
     std::transform(/*todo*/ dummy.begin(), /*todo*/ dummy.end(),
                    out.begin(),
                    [/* todo */](int i) { return /* todo */ 0; });
