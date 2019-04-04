6cf29fa496d73fd4b0a4011516cc2604a7254319
Chenzhuo Zhu <markzhu95@gmail.com>
2019-04-04 13:57:58 -0700

    Remove gcp vm scripts. They will go to starter code.

--- gcp/login_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Config zone.
-gcloud config set compute/zone us-west1-b
-
-gcloud compute instances start gpu-instance
-gcloud compute ssh gpu-instance
--- gcp/stop_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Config zone.
-gcloud config set compute/zone ${ZONE}
-
-# Stop gpu-instance
-gcloud compute instances stop gpu-instance
